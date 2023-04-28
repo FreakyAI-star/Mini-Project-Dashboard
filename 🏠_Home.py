c61d63aeed61e7c0cfacb2208ac133a0a0a96995
surbhi dua <104771978+surbhi-dua-drishya@users.noreply.github.com>
2023-04-28 17:19:45 +0530

    background added to home

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -8,31 +8,68 @@ st.set_page_config(page_title="Dashboard", page_icon="")
 
 import streamlit as st
 
-# Set page title
-st.title("Stock Market Dashboard")
+import streamlit as st
 
-# Create a header for the page
-st.header("Welcome to our stock market analysis tool")
+# Define some CSS styles for the title and content
+import streamlit as st
+page_bg_img = '''
+<style>
+
+</style>
+'''
+import base64
+def add_bg_from_local(image_file):
+    with open(image_file, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read())
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        background-size: cover
+    
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+add_bg_from_local('back.png')
+# Use the st.markdown function to display the styled CSS
+st.markdown(page_bg_img, unsafe_allow_html=True)
+# Define some CSS styles for the title and content
+title_style = """
+    <style>
+    /* Align title to the left */
+   .css-uf99v8 {
+   
+    align-items: baseline !important;
+    margin-top: 7rem;
+}
+h1 {
+
+    color: springgreen !important;
+    padding: 0 !important;
+    font-size: 86px;
+  
+}
+.css-1n76uvr {
+
+    padding-left: 4rem !important;
+}
+.css-1we6k59 {
+    
+    margin-bottom: 20px !important;
+   
+}
+    </style>
+"""
+content_style = "<div style='font-size: 36px;'>Data Analysis Tool</div>"
+
+# Use the st.markdown function to display the styled title and content
+st.markdown(title_style, unsafe_allow_html=True)
+st.title("StockPilot")
+st.markdown(content_style, unsafe_allow_html=True)
+
+
 
 # Create a subheader with bold and left-aligned text
-st.subheader("**Research**")
-
-# Add some descriptive text for the Research section
-st.write("Our research section provides in-depth analysis of key stocks and market sectors, helping you stay informed and make informed investment decisions.")
-
-# Create another subheader for the Prediction section
-st.subheader("**Prediction**")
-
-# Add some descriptive text for the Prediction section
-st.write("Our prediction section uses advanced modeling techniques to forecast stock prices and identify emerging trends, giving you a competitive edge in the market.")
-
-# Align all text to the left side of the page
-st.set_option('deprecation.showfileUploaderEncoding', False)
-
-# Hide Streamlit menu
-hide_menu_style = """
-        <style>
-        #MainMenu {visibility: hidden;}
-        </style>
-        """
-st.markdown(hide_menu_style, unsafe_allow_html=True)
