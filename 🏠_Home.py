7f936227893bfd090cdcc98da4b0b8718d2946ff
surbhi dua <104771978+surbhi-dua-drishya@users.noreply.github.com>
2023-04-28 16:03:48 +0530

    home

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -6,41 +6,33 @@ import streamlit.components.v1 as components
 st.set_page_config(page_title="Dashboard", page_icon="")
 
 
-# Replace "your_code" with your actual font awesome code
+import streamlit as st
 
-# List of pages available for display
-st.title('Alfred - Home')
-with st.spinner("Loading About ..."):
-    st.markdown(
+# Set page title
+st.title("Stock Market Dashboard")
+
+# Create a header for the page
+st.header("Welcome to our stock market analysis tool")
+
+# Create a subheader with bold and left-aligned text
+st.subheader("**Research**")
+
+# Add some descriptive text for the Research section
+st.write("Our research section provides in-depth analysis of key stocks and market sectors, helping you stay informed and make informed investment decisions.")
+
+# Create another subheader for the Prediction section
+st.subheader("**Prediction**")
+
+# Add some descriptive text for the Prediction section
+st.write("Our prediction section uses advanced modeling techniques to forecast stock prices and identify emerging trends, giving you a competitive edge in the market.")
+
+# Align all text to the left side of the page
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+# Hide Streamlit menu
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
         """
-        This Streamlit app is a Financial data dashbord that could be used
-        for data visualization, exploration and predicting behavior of
-        Financial quantities.""", unsafe_allow_html=True,
-       
-    )
-st.markdown(
-'<style>'+open('style.css').read()+'</style>'
-       ,
-        unsafe_allow_html=True,
- )
-
-    
-    # Initialize a variable to keep track of which tab is currently active
-    # active_tab = "Home"
-    
-    # # Create buttons in the sidebar for each page
-    # if st.sidebar.button("Home", key="home") and active_tab != "Home":
-    #     active_tab = "Home"
-    # if st.sidebar.button("Research", key="research") and active_tab != "Research":
-    #     active_tab = "Research"
-    # if st.sidebar.button("Prediction", key="prediction") and active_tab != "Prediction":
-    #     active_tab = "Prediction"
-    
-    # # Show the active tab content
-    # if active_tab == "Home":
-    #     ast.shared.components.write_page(PAGES["Home"])
-    # elif active_tab == "Research":
-    #     ast.shared.components.write_page(PAGES["Research"])
-    # elif active_tab == "Prediction":
-    #     ast.shared.components.write_page(PAGES["Prediction"])
-
+st.markdown(hide_menu_style, unsafe_allow_html=True)
